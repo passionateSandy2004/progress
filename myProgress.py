2a0d1d44b7df4da3787d00ecf5eea9c8a679f286
passionateSandy2004 <152080198+passionateSandy2004@users.noreply.github.com>
2024-07-17 16:20:11 +0530

    Add files via upload

---
+++ myProgress.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from st_circular_progress import CircularProgress
+import pandas as pd 
+
+c1,c2=st.columns(2)
+def change():
+    df1 = pd.read_json('data.json', typ='series')
+    data_dict = df1.to_dict()
+    cp1.update_value(progress=data_dict["HR"])
+    cp2.update_value(progress=data_dict["CAL"])
+with c1:
+    cp1 = CircularProgress(
+            value=0,
+            label="Heart Rate",
+            size="Large",
+            key="hr",
+        )
+    cp1.st_circular_progress()
+with c2:
+    cp2 = CircularProgress(
+            value=0,
+            label="Calories",
+            size="Large",
+            key="cal",
+        )
+    cp2.st_circular_progress()
+
+hr=st.number_input("Heart Rate:")
+cal=st.number_input("Calories:")
+if hr or cal:
+    df = pd.DataFrame([{"HR":hr, "CAL":cal}])
+    df.to_json('data.json', orient='records', lines=True)
+    change()
+st.button("Refresh")
+
+
